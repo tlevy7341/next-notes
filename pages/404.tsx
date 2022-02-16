36fb16384e29258fb78e6ede0fd9d50c564528f5
Tyrail Leverett <tyrailleverett@gmail.com>
2022-02-16 11:48:56 -0600

    updated title for 404

--- pages/404.tsx
+++ pages/404.tsx
@@ -7,7 +7,7 @@ const Custom404: NextPage = () => {
   return (
     <>
       <Head>
-        <title>The page you were looking for doesn't exist | 404</title>
+        <title>404</title>
       </Head>
       <div className="w-screen h-screen flex flex-col justify-center items-center bg-blue-gray-800 text-cool-gray-100">
         <h2 className="font-bold text-2xl pb-3">404 Page Not Found</h2>
